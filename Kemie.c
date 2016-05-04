d65a6570765d3c394170553cc54394234e8ce630
Taker <Taker>
2016-05-04 23:43:32 +0200

    Implemented menu rendering

--- Kemie.c
+++ Kemie.c
@@ -1,56 +1,48 @@
-/*****************************************************************/
-/*                                                               */
-/*   CASIO fx-9860G SDK Library                                  */
-/*                                                               */
-/*   File name : [ProjectName].c                                 */
-/*                                                               */
-/*   Copyright (c) 2006 CASIO COMPUTER CO., LTD.                 */
-/*                                                               */
-/*****************************************************************/
 #include "fxlib.h"
 
-
-//****************************************************************************
-//  AddIn_main (Sample program main function)
-//
-//  param   :   isAppli   : 1 = This application is launched by MAIN MENU.
-//                        : 0 = This application is launched by a strip in eACT application.
-//
-//              OptionNum : Strip number (0~3)
-//                         (This parameter is only used when isAppli parameter is 0.)
-//
-//  retval  :   1 = No error / 0 = Error
-//
-//****************************************************************************
 int AddIn_main(int isAppli, unsigned short OptionNum)
 {
-    unsigned int key;
+	unsigned int key;
 
-    Bdisp_AllClr_DDVRAM();
+	render();
+	
+	while(1)
+	{
+		GetKey(&key);
+	}
 
-    locate(1,4);
-    Print((unsigned char*)"This application is");
-    locate(1,5);
-    Print((unsigned char*)" sample Add-In.");
-
-    while(1){
-        GetKey(&key);
-    }
-
-    return 1;
+	return 1;
 }
 
-
-
-
-//****************************************************************************
-//**************                                              ****************
-//**************                 Notice!                      ****************
-//**************                                              ****************
-//**************  Please do not change the following source.  ****************
-//**************                                              ****************
-//****************************************************************************
-
+int render()
+{
+	int i;
+	
+	Bdisp_AllClr_DDVRAM();
+	for(i = 0; i < 64; i++) Bdisp_SetPoint_VRAM(21, i, 1);
+	for(i = 0; i < 22; i++) 
+	{
+		Bdisp_SetPoint_VRAM(i, 0, 1);
+		Bdisp_SetPoint_VRAM(i, 21, 1);
+		Bdisp_SetPoint_VRAM(i, 42, 1);
+		Bdisp_SetPoint_VRAM(i, 63, 1);
+	}
+	
+	PrintMini(1, 8, "N,O,F", 0);
+	PrintMini(5, 30, "NA", 0); PrintMini(15, 28, "+", 0);
+	for(i = 0; i < 7; i++) Bdisp_SetPoint_VRAM(7 + i, 60, 1);
+	for(i = 0; i < 3; i++)
+	{
+		Bdisp_SetPoint_VRAM(4 + i, 54 + 2 * i, 1);
+		Bdisp_SetPoint_VRAM(4 + i, 55 + 2 * i, 1);
+		Bdisp_SetPoint_VRAM(16 - i, 54 + 2 * i, 1);
+		Bdisp_SetPoint_VRAM(16 - i, 55 + 2 * i, 1);
+	}
+	Bdisp_SetPoint_VRAM(5, 53, 1); Bdisp_SetPoint_VRAM(6, 53, 1); Bdisp_SetPoint_VRAM(14, 53, 1); Bdisp_SetPoint_VRAM(15, 53, 1);
+	Bdisp_SetPoint_VRAM(7, 52, 1); Bdisp_SetPoint_VRAM(13, 52, 1);
+	Bdisp_SetPoint_VRAM(8, 51, 1); Bdisp_SetPoint_VRAM(9, 51, 1); Bdisp_SetPoint_VRAM(11, 51, 1); Bdisp_SetPoint_VRAM(12, 51, 1);
+	Bdisp_SetPoint_VRAM(10, 50, 1);
+}
 
 #pragma section _BR_Size
 unsigned long BR_Size;
@@ -58,20 +50,8 @@ unsigned long BR_Size;
 
 
 #pragma section _TOP
-
-//****************************************************************************
-//  InitializeSystem
-//
-//  param   :   isAppli   : 1 = Application / 0 = eActivity
-//              OptionNum : Option Number (only eActivity)
-//
-//  retval  :   1 = No error / 0 = Error
-//
-//****************************************************************************
 int InitializeSystem(int isAppli, unsigned short OptionNum)
 {
     return INIT_ADDIN_APPLICATION(isAppli, OptionNum);
 }
-
 #pragma section
-
