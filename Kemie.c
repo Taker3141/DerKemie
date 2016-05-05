b5a1a616b3609a48e61963e2a71272b8c8103836
Taker <Taker>
2016-05-05 23:07:38 +0200

    Implemented rendering of atoms and their electrons

--- Kemie.c
+++ Kemie.c
@@ -1,14 +1,51 @@
 #include "fxlib.h"
 
+typedef struct
+{
+	char number;
+	char charge;
+	char x, y;
+	union
+	{
+		unsigned char eByte;
+		struct
+		{
+			unsigned char t:2;
+			unsigned char r:2;
+			unsigned char b:2;
+			unsigned char l:2;
+		}e;
+	}	
+	Electrons;
+}
+Atom;
+
+int selected = 0;
+int displayAtoms = 0;
+int camX = 0, camY = 0;
+Atom atoms[2];
+
 int AddIn_main(int isAppli, unsigned short OptionNum)
 {
 	unsigned int key;
 
+	atoms[0].number =  1; atoms[0].charge = 0; atoms[0].x = 3; atoms[0].y = 1; atoms[0].Electrons.eByte = 0x01;
+	atoms[1].number = 10; atoms[1].charge = 0; atoms[1].x = 3; atoms[1].y = 0; atoms[1].Electrons.eByte = 0xFF;
+	atoms[2].number =  7; atoms[2].charge = 0; atoms[2].x = 2; atoms[2].y = 1; atoms[2].Electrons.eByte = 0x57;
 	render();
 	
 	while(1)
 	{
 		GetKey(&key);
+		if(key == KEY_CTRL_UP && selected > 0) selected--;
+		if(key == KEY_CTRL_DOWN && selected < 2) selected++;
+		if(key == KEY_CTRL_EXE) 
+		{
+			if(!displayAtoms) displayAtoms = 1;
+			else displayAtoms = 0;
+		}
+		if(key == KEY_CTRL_EXIT) displayAtoms = 0;
+		render();
 	}
 
 	return 1;
@@ -18,7 +55,7 @@ int render()
 {
 	int i;
 	
-	Bdisp_AllClr_DDVRAM();
+	Bdisp_AllClr_VRAM();
 	for(i = 0; i < 64; i++) Bdisp_SetPoint_VRAM(21, i, 1);
 	for(i = 0; i < 22; i++) 
 	{
@@ -42,6 +79,41 @@ int render()
 	Bdisp_SetPoint_VRAM(7, 52, 1); Bdisp_SetPoint_VRAM(13, 52, 1);
 	Bdisp_SetPoint_VRAM(8, 51, 1); Bdisp_SetPoint_VRAM(9, 51, 1); Bdisp_SetPoint_VRAM(11, 51, 1); Bdisp_SetPoint_VRAM(12, 51, 1);
 	Bdisp_SetPoint_VRAM(10, 50, 1);
+	
+	for(i = 0; i < 22; i++) Bdisp_SetPoint_VRAM(22, i + selected * 21, 1);
+	if(displayAtoms)
+	{
+		for(i = 0; i < 106; i++) 
+		{
+			Bdisp_SetPoint_VRAM(22 + i, selected * 21, 1);
+			Bdisp_SetPoint_VRAM(22 + i, (selected + 1) * 21, 1);
+		}
+		for(i = 0; i < 22; i++) Bdisp_SetPoint_VRAM(127, i + selected * 21, 1);
+	}
+	
+	renderAtom(atoms[0]);
+	renderAtom(atoms[1]);
+	renderAtom(atoms[2]);
+}
+
+char* symbols[] = {"H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar"};
+int renderAtom(Atom a)
+{
+	int i;
+	int posX = (a.x * 15) - camX;
+	int posY = 63 - (a.y * 15) - camY;
+	if(posX < -15 || posY < -15 || posX > 128 || posY > 64) return;
+	if(symbols[a.number - 1][1] != 0) PrintXY(posX + 1, posY - 10, symbols[a.number - 1], 0);
+	else PrintXY(posX + 4, posY - 10, symbols[a.number - 1], 0);
+	
+	if(a.Electrons.e.t == 1 || a.Electrons.e.t == 2) Bdisp_SetPoint_VRAM(posX + 7, posY - 14, 1);
+	if(a.Electrons.e.t == 3) for(i = 0; i < 9; i++) {Bdisp_SetPoint_VRAM(posX + 3 + i, posY - 14, 1);};
+	if(a.Electrons.e.r == 1 || a.Electrons.e.r == 2) Bdisp_SetPoint_VRAM(posX + 14, posY - 7, 1);
+	if(a.Electrons.e.r == 3) for(i = 0; i < 9; i++) {Bdisp_SetPoint_VRAM(posX + 14, posY - 3 - i, 1);};
+	if(a.Electrons.e.b == 1 || a.Electrons.e.b == 2) Bdisp_SetPoint_VRAM(posX + 7, posY, 1);
+	if(a.Electrons.e.b == 3) for(i = 0; i < 9; i++) {Bdisp_SetPoint_VRAM(posX + 3 + i, posY, 1);};
+	if(a.Electrons.e.l == 1 || a.Electrons.e.l == 2) Bdisp_SetPoint_VRAM(posX, posY - 7, 1);
+	if(a.Electrons.e.l == 3) for(i = 0; i < 9; i++) {Bdisp_SetPoint_VRAM(posX, posY - 3 - i, 1);};
 }
 
 #pragma section _BR_Size
